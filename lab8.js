b0abe0e1eb4d71c61268f4f49907b2a6edc2dc3b
Santiago Acosta <santiago.acoes.4@gmail.com>
2026-02-19 22:47:48 -0600

    lab8 problema stock

--- lab8.js
+++ lab8.js
@@ -1,3 +1,22 @@
-const filesystem= require("fs");
-filesystem.writeFileSync("lab8.txt","Si se logro la tarea");
-console.log("ya se escribio en el archivo jajaja")
+const productos = [
+    { nombre: "Teclado RGB", stock: 12 },
+    { nombre: "Mouse Óptico", stock: 3 },
+    { nombre: "Monitor 4K", stock: 7 },
+    { nombre: "Cables HDMI", stock: 2 },
+    { nombre: "Audífonos", stock: 15 }
+];
+
+let totalProductos = 0;
+
+console.log("Reporte de Stock");
+
+for (let i = 0; i < productos.length; i++) {
+    let productoActual = productos[i];
+    totalProductos += productoActual.stock;
+
+    if (productoActual.stock < 5) {
+        console.log(`ALERTA: El producto "${productoActual.nombre}" solo tiene ${productoActual.stock} unidades.`);
+    }
+}
+
+console.log(`Total de artículos en bodega: ${totalProductos}`);
