ec899ce2f07f9c717e6974a2018c6f6a92e2f2f5
Santiago Acosta <santiago.acoes.4@gmail.com>
2026-02-19 21:41:38 -0600

    Lab 8 sumatoria

---
+++ lab8.js
@@ -0,0 +1,10 @@
+console.log("hola");
+
+const arreglo=[500, 60, 90, 100, 10, 20,]
+let suma=0
+for (let item of arreglo){
+    suma+=item
+    }
+let promedio=suma/arreglo.length
+console.log("La sumatoria es:", suma);
+console.log("El promedio es:", promedio);
